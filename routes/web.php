e2342df3dd076a0c6ec5deb421b852cd64c0a453
iHDeveloper <hamzadeveloper@hotmail.com>
2018-04-22 14:31:23 +0300

    Change the services get to post

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,6 @@ Route::get("/{id}/login/{token?}", "PanelController@login")->name("panel.login")
 Route::get("/{id}/open/{token?}", "PanelController@open")->name("panel.open");
 Route::get("/{id}/@{page}/{token?}", "PanelController@page")->name("panel.page");
 Route::get("/{id}/$@{route}/{token?}", "PanelController@route")->name("panel.route");
-Route::get("/@{id}/@services/@nickname", "ServicesController@nickname")->name("service.nickname");
+Route::post("/@{id}/@services/@nickname", "ServicesController@nickname")->name("service.nickname");
 Route::get("oauth2/discord/{id?}", "OAuth2Controller@discord")->name("oauth2.discord");
 Route::get("devices/auth", "DevicesController@auth");
