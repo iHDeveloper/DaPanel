88e83dad0dac29bbd507d5538f676ffe805148fe
iHDeveloper <hamzadeveloper@hotmail.com>
2018-04-22 06:38:25 -0400

    Update!

--- git-pull-adapter.sh
+++
@@ -1,8 +0,0 @@
-echo "[GitPull-Adapter] Started! Ctrl+C to stop [Every 5 seconds]"
-while true
-do
-	echo "[GitPull-Adapter] Updating master using git"
-	git pull github master
-	echo "[GitPull-Adapter] Updated! master brunch using git [ 5 seconds waiting ]"
-	sleep 5
-done
