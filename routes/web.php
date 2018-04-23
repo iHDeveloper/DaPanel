f66708edae846d36b249a0f03e1c3d6a5a907f74
iHDeveloper <hamzadeveloper@hotmail.com>
2018-04-23 08:09:52 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -11,7 +11,6 @@
 |
 */
 Route::get("/", function(){return view("layout.app");});
-Route::get("/{id}", "PanelController@panel")->name("panel.find");
 Route::get("/{id}/login/{token?}", "PanelController@login")->name("panel.login");
 Route::get("/{id}/open/{token?}", "PanelController@open")->name("panel.open");
 Route::get("/{id}/@{page}/{token?}", "PanelController@page")->name("panel.page");
@@ -24,3 +23,4 @@ Route::prefix("/@studio")->group(function(){
     Route::get("/{id}/editor", "StudioController@editor");
     Route::get("/{id}/app", "StudioController@app");
 });
+Route::get("/{id}", "PanelController@panel")->name("panel.find");
