ad0174b59f2e519012534d884f4a28cf91703296
iHDeveloper <hamzadeveloper@hotmail.com>
2018-04-23 08:14:17 +0300

    Change: Login Button Text

--- resources/views/studio/default.blade.php
+++ resources/views/studio/default.blade.php
@@ -27,8 +27,7 @@
             <li><a href="#">Page 2</a></li>
             </ul>
             <ul class="nav navbar-nav navbar-right">
-            <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li>
-            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li>
+            <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login with discord</a></li>
             </ul>
         </div>
     </nav>
