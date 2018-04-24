c3a299f4c2ce25790431219e9ee0065b42e2a85d
iHDeveloper <hamzadeveloper@hotmail.com>
2018-04-24 17:27:01 +0300

    Add: @asset route

--- routes/web.php
+++ routes/web.php
@@ -11,17 +11,18 @@
 |
 */
 Route::get("/", function(){return view("layout.app");});
-Route::get("/{id}/login/{token?}", "PanelController@login")->name("panel.login");
-Route::get("/{id}/open/{token?}", "PanelController@open")->name("panel.open");
-Route::get("/{id}/@{page}/{token?}", "PanelController@page")->name("panel.page");
-Route::get("/{id}/$@{route}/{token?}", "PanelController@route")->name("panel.route");
-Route::post("/@{id}/@services/@nickname", "ServicesController@nickname")->name("service.nickname");
+Route::get("{id}/login/{token?}", "PanelController@login")->name("panel.login");
+Route::get("{id}/open/{token?}", "PanelController@open")->name("panel.open");
+Route::get("{id}/@{page}/{token?}", "PanelController@page")->name("panel.page");
+Route::get("{id}/$@{route}/{token?}", "PanelController@route")->name("panel.route");
+Route::post("@{id}/@services/@nickname", "ServicesController@nickname")->name("service.nickname");
 Route::get("oauth2/discord/{id?}", "OAuth2Controller@discord")->name("oauth2.discord");
 Route::get("devices/auth", "DevicesController@auth");
-Route::prefix("/@studio")->group(function(){
+Route::get("@{asset}", "AssestController@callback");
+Route::prefix("@studio")->group(function(){
     Route::get("/", "StudioController@home");
-    Route::get("/editor", "StudioController@editor");
-    Route::get("/{id}/editor", "StudioController@editor");
-    Route::get("/{id}/app", "StudioController@app");
+    Route::get("editor", "StudioController@editor");
+    Route::get("{id}/editor", "StudioController@editor");
+    Route::get("{id}/app", "StudioController@app");
 });
-Route::get("/{id}", "PanelController@panel")->name("panel.find");
+Route::get("{id}", "PanelController@panel")->name("panel.find");
