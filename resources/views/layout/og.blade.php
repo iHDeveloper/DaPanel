75a4ccd8def48476564d6947c1424a2e6e7d537c
iHDeveloper <hamzadeveloper@hotmail.com>
2018-04-25 15:22:48 +0300

    Add: Open Graph Protocol

---
+++ resources/views/layout/og.blade.php
@@ -0,0 +1,3 @@
+<meta name="og:title" content="DaPanel | Discord Panel">
+<meta name="og:type" content="Discord Panel">
+<meta name="og:description" content="Wants to control your futures from website? We can do that for you.">
