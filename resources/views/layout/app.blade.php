e6f1b5c7d0e66088bceaad6cd6cdafc554ae9572
iHDeveloper <hamzadeveloper@hotmail.com>
2018-04-25 15:31:13 +0300

    Fix: Extends The Open Graph Protocol

--- resources/views/layout/app.blade.php
+++ resources/views/layout/app.blade.php
@@ -9,8 +9,7 @@
         <title>DaPanel</title>
         <meta name="description" content="">
         <meta name="viewport" content="width=device-width, initial-scale=1">
-        {{ str_replace('"', '', view('layout.og')) }}
-        {{ dd(view('layout.og')) }}
+        @extends('layout.og')
         <link rel="apple-touch-icon" href="{{asset('images/apple-touch-icon.png')}}">
         <link rel="stylesheet" href="{{asset('css/fonticons.css')}}">
         <link rel="stylesheet" href="{{asset('fonts/stylesheet.css')}}">
--- resources/views/studio/home.blade.php
+++ resources/views/studio/home.blade.php
@@ -5,7 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <title>DaPanel: Studio</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    {{ view('layout.og')}}
+    @extends('layout.og')
     <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
