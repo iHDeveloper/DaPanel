ad3c772340c28db527fe95ef88ccd25c113bc01f
iHDeveloper <hamzadeveloper@hotmail.com>
2018-04-25 14:33:12 +0300

    Fix: Routes System

--- routes/web.php
+++ routes/web.php
@@ -10,18 +10,28 @@
 | contains the "web" middleware group. Now create something great!
 |
 */
+
+# Others
 Route::get("/", function(){return view("layout.app");});
-Route::get("{id}/login/{token?}", "PanelController@login")->name("panel.login");
-Route::get("{id}/open/{token?}", "PanelController@open")->name("panel.open");
-Route::get("{id}/@{page}/{token?}", "PanelController@page")->name("panel.page");
-Route::get("{id}/$@{route}/{token?}", "PanelController@route")->name("panel.route");
-Route::post("@{id}/@services/@nickname", "ServicesController@nickname")->name("service.nickname");
-Route::get("oauth2/discord/{id?}", "OAuth2Controller@discord")->name("oauth2.discord");
 Route::get("devices/auth", "DevicesController@auth");
+
+# OAuth2
+Route::get("oauth2/discord/{id?}", "OAuth2Controller@discord")->name("oauth2.discord");
+
+# Services
+Route::post("@{id}/@services/@nickname", "ServicesController@nickname")->name("service.nickname");
+
+# Studio Group
 Route::prefix("@studio")->group(function(){
     Route::get("/", "StudioController@home");
     Route::get("editor", "StudioController@editor");
     Route::get("{id}/editor", "StudioController@editor");
     Route::get("{id}/app", "StudioController@app");
 });
+
+# Panel Urls
 Route::get("{id}", "PanelController@panel")->name("panel.find");
+Route::get("{id}/login/{token?}", "PanelController@login")->name("panel.login");
+Route::get("{id}/open/{token?}", "PanelController@open")->name("panel.open");
+Route::get("{id}/@{page}/{token?}", "PanelController@page")->name("panel.page");
+Route::get("{id}/$@{route}/{token?}", "PanelController@route")->name("panel.route");
