67672bdd5335158f152aa888de00642f15c2990c
iHDeveloper <hamzadeveloper@hotmail.com>
2018-04-26 16:59:50 +0300

    Add: Basics And WebSocket

---
+++ public/js/studio.js
@@ -0,0 +1,38 @@
+var log = console.log;
+var application = {
+    websocket: null,
+    onsocketconnect: function() {
+        log("Connecting...");
+    },
+    onsocketopen: function() {
+        log("Connected!");
+    },
+    onsocketerror: function() {
+        log("Failed to connect!");
+    },
+    onsocketclose: function() {
+        log("Disconnected!");
+    },
+    oninput: function() {},
+    onping: function() {},
+    onlogin: function() {},
+};
+
+$(document).ready(function() {
+    setTimeout(function() {
+        application.onsocketconnect();
+        application.websocket = new WebSocket("ws://dapanel.tk:3020");
+        application.websocket.onopen = function() {
+            application.onsocketopen();
+        };
+        application.websocket.onerror = function() {
+            application.onsocketerror();
+        }
+        application.websocket.onmessage = function(data) {
+            console.log(data);
+        };
+        application.websocket.onclose = function() {
+            applicationCache.onsocketclose();
+        }
+    }, 100);
+});
