e6a887116288421a4b50f370cba507398d9008af
iHDeveloper <hamzadeveloper@hotmail.com>
2018-04-29 08:58:16 +0300

    Add: File Explorer

--- resources/views/studio/editor.blade.php
+++ resources/views/studio/editor.blade.php
@@ -3,22 +3,28 @@
 <style src="{{asset('css/studio.css')}}"></style>
 @endsection
 @section('content')
-<nav class="navbar navbar-default">
+<nav class="navbar navbar-default navbar-fixed-top studio-navbar">
 	<div class="container-fluid">
-		<div class="navbar-header">
+		<div class="navbar-header studio-center-navbar">
 			<select id="branch">
-				<option>"<master>"</option>
+				<option><master></option>
 			</select>
 		</div>
-		<ul class="nav navbar-nav navbar-right">
-			<li><a><span class="label label-success">Connect</span></a></li>
+		<ul class="nav navbar-nav navbar-right studio-center-navbar studio-right-navbar">
+			<li><button>Connect</button></li>
 		</ul>
 	<div>
 </nav>
 
 <div class="row">
 	<div class="col-md-4">
-		
+		<ul class="list-group">
+			<li><button>.layout</button></li>
+			<li><button>.config</button></li>
+			<li><button>.branch</button></li>
+			<li><button>index.page</button></li>
+			<li><button>home.route</button></li>
+		</ul>
 	</div>
 	<div class="col-md-6">
 	</div>
