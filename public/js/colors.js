dc82223ccbfa69dacfdcac38d69c83445abd4f87
iHDeveloper <hamzadeveloper@hotmail.com>
2018-05-01 15:17:02 +0300

    Fix: undefined bug

--- public/js/colors.js
+++ public/js/colors.js
@@ -1,3 +1,3 @@
 console.debug = function(name, message, arg1) {
-    console.log("%c[" + name + "] %c", 'color:purple', 'color:black', message, arg1);
+    console.log("%c[" + name + "] %c", 'color:purple', 'color:black', message, (arg1 == null ? "" : arg1));
 };
