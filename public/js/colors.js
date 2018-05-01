2697d34c7fab66e0a29168918618dc57bafbc887
iHDeveloper <hamzadeveloper@hotmail.com>
2018-05-01 14:45:29 +0300

    Support: More args

--- public/js/colors.js
+++ public/js/colors.js
@@ -1,3 +1,3 @@
-console.debug = function(name, message) {
-    console.log("%c[" + name + "] %c", 'color:purple', 'color:black', message);
+console.debug = function(name, message, arg1) {
+    console.log("%c[" + name + "] %c", 'color:purple', 'color:black', message, arg1);
 };
