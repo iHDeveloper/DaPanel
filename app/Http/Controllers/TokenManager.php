1289e8b66cadd95b60f55dae2caa2b27b9027bc4
iHDeveloper <hamzadeveloper@hotmail.com>
2018-05-16 22:15:05 +0300

    Update TokenManager with the new System

--- app/Http/Controllers/TokenManager.php
+++ app/Http/Controllers/TokenManager.php
@@ -10,13 +10,10 @@ class TokenManager extends Controller
     
     public static function auth(Request $req, $token, $id){
         if($token == null) return null;
-        $where = AuthProfile::where('token', $token);
+        $where = AuthProfile::find($token);
         if($where->count() <= 0) return null;
         $profile = $where->get()[0];
-        $clientip = $req->ip();
-        if($clientip != $profile->ip) return null;
         if($id != $profile['panel_id']) return null;
-        if($profile->clientid == -1) return null;
         $profile->save();
         return $profile;
     }
